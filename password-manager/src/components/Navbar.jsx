fcaf0fc6021d5e68d668f3a2bd934aa93f216074
MisbahuddinSyed <syedmisbahuddin2006@gmail.com>
2026-09-08 01:03:56 +0530

    Initialized app with a background on creating ui

---
+++ password-manager/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='bg-purple-300'>
+        <div className="logo">PassOP</div>
+    </nav>
+  )
+}
+
+export default Navbar
