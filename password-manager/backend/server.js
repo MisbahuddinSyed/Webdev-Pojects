6a81458ccc9a9b713b7421e607df273b8c1683fc
MisbahuddinSyed <syedmisbahuddin2006@gmail.com>
2026-09-26 18:56:57 +0530

    Apply API endpoints for POST , GET and DELETE

--- password-manager/backend/server.js
+++ password-manager/backend/server.js
@@ -5,27 +5,39 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
-app.use(express.json);
+app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
-mongoose.connect(process.env.MONGO_URI).then(()=>{
-    console.log("Database connected")
-})
-.catch((err) => {
-        console.log("MongoDB connection error:", err);
-    });
+
 
 const passwordSchema = new mongoose.Schema({
     website: String,
     username: String,
     password: String
-
+    
 })
-
 const Password = mongoose.model("Password", passwordSchema)
 
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("Database connected")
+    app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+})
+.catch((err) => {
+    console.log("MongoDB connection error:", err);
+});
+
 app.get('/', async(req, res)=>{
-    res.send(req.body)
+    const result = await Password.find();
+    res.json(result);
+})
+
+app.post('/', async(req, res) =>{
+    const password = await Password.create(req.body);
+    res.status(201).json(password);
+})
+
+app.delete('/:id', async(req, res) => {
+    const password = await Password.findByIdAndDelete(req.params.id);
+    res.json(!!password);
 })
